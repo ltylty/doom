6cd809a5cf0d4a3f9ba40827ff535c4a71d5b361
liutianyuan <liutianyuan1737@stnts.com>
2023-07-27 08:56:10 +0800

    first commit

---
+++ config.el
@@ -0,0 +1,167 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-dracula)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+;; (setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(add-hook 'window-setup-hook #'toggle-frame-maximized)
+;; (add-hook 'window-setup-hook #'recentf-mode)
+;; (add-hook 'window-setup-hook #'global-visual-line-mode)
+(+global-word-wrap-mode +1)
+
+;; (find-file "e:/OneDrive/note/stnts/todo.org")
+
+;; (map! :leader
+;;       :desc "last buffer"
+;;       "TAB" #'evil-switch-to-windows-last-buffer)
+
+(add-to-list 'process-coding-system-alist
+                        '("[rR][gG]" . (utf-8 . gbk-dos)))
+
+(after! consult
+  (setq
+   consult-async-min-input 3
+   consult-async-input-debounce 0.4))
+
+(setq-hook! 'prog-mode-hook comment-line-break-function nil)
+(set-selection-coding-system 'utf-16le-dos)
+
+;; company
+(add-hook 'shell-mode-hook (lambda() (company-mode 0)))
+(add-hook 'eshell-mode-hook (lambda() (company-mode 0)))
+;; (set-company-backend! 'prog-mode 'company-tabnine 'company-dabbrev-code)
+;; (set-company-backend! 'text-mode 'company-tabnine 'company-dabbrev)
+(set-company-backend! 'prog-mode 'company-capf 'company-dabbrev-code)
+(set-company-backend! 'text-mode 'company-capf 'company-dabbrev)
+(after! company
+  (setq
+   company-dabbrev-code-ignore-case t
+   company-dabbrev-ignore-case t))
+
+(require 'tabnine)
+(add-to-list 'prog-mode-hook #'tabnine-mode)
+(add-to-list 'completion-at-point-functions #'tabnine-completion-at-point)
+
+;; Centaur-tabs
+(use-package centaur-tabs
+  :bind
+  ("C-<prior>" . centaur-tabs-backward)
+  ("C-<next>" . centaur-tabs-forward))
+(setq centaur-tabs-set-bar 'under)
+(map! :leader
+      :desc "kill other tabs"
+      "b o" #'centaur-tabs-kill-other-buffers-in-current-group)
+
+;; sql-formatter
+(setq sqlformat-command 'sql-formatter)
+(setq sqlformat-args '("-l" "bigquery"))
+(map! :after sql
+      :map sql-mode-map
+      "C-c C-f" #'sqlformat)
+
+;; neotree
+;; (setq neo-window-fixed-size nil)
+;; (map! :map neotree-mode-map
+;;       :n ">" #'evil-window-increase-width
+;;       :n "<" #'evil-window-decrease-width)
+
+;; treemacs
+(map! :leader
+      :desc "Select treemacs"
+      "0" #'treemacs-select-window)
+(map! :leader
+      :desc "treemacs current project"
+      "o t" #'treemacs-add-and-display-current-project-exclusively)
+(after! treemacs
+  (setq treemacs-collapse-dirs 3)
+  (treemacs-project-follow-mode t))
+
+;; evil
+(require 'evil-textobj-line)
+(use-package evil
+  :custom
+  evil-disable-insert-state-bindings t
+  )
+(setq evil-emacs-state-cursor 'hbar)
+(setq-default cursor-type 'hbar)
+(setq doom-modeline-modal-icon nil)
+;; (add-hook! 'magit-status-mode-hook 'evil-emacs-state)
+;; (add-hook! 'magit-log-mode-hook 'evil-emacs-state)
+;; (define-key! :keymaps +default-minibuffer-maps
+;;     [escape] #'abort-recursive-edit)
+;; (define-key evil-emacs-state-map (kbd "<escape>") 'evil-normal-state)
+;; (add-to-list 'evil-emacs-state-modes 'magit-status-mode)
+;; (add-to-list 'evil-emacs-state-modes 'dired-mode)
+;; (add-to-list 'evil-emacs-state-modes 'treemacs-mode)
+;; (add-to-list 'evil-emacs-state-modes 'ediff-mode)
