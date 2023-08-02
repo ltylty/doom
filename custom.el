016f41a8e70e6f7f9e06c7c0e9f8e12eddd966be
liutianyuan <liutianyuan1737@stnts.com>
2023-08-02 13:40:19 +0800

    remove format

--- custom.el
+++ custom.el
@@ -3,10 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(codeium/metadata/api_key "80c24485-4611-4482-bdbe-0cf9c3a2df68")
- ;; '(consult-async-input-debounce 0.5)
- ;; '(ignored-local-variable-values '((git-commit-major-mode . git-commit-elisp-text-mode)))
- ;; '(package-selected-packages '(evil-better-visual-line sql-indent))
  )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
