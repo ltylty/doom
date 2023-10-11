e9bd537cc3a8a604aef54fc152a60d248ff5cd0d
liutianyuan <liutianyuan1737@stnts.com>
2023-10-11 18:03:40 +0800

    format code

--- config.el
+++ config.el
@@ -23,13 +23,11 @@
 ;;
 ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;(setq doom-font (font-spec :family "FiraCode Nerd Font Mono Bold" :size 13))
-(setq doom-font (font-spec :family "Maple Mono SC NF Bold"))
-;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
+(setq doom-font (font-spec :family "Maple Mono SC NF Bold"))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
